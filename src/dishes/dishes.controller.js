d0f10406973fbb5ca94528f478e1344bdbc125c5
Linh <lcahu91@gmail.com>
2021-08-16 17:01:15 -0400

    all dishes tests passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -29,13 +29,24 @@ function hasRequiredFields(req, res, next) {
 function priceGreaterThanZero(req, res, next) {
   const { data: { price } = {} } = req.body;
 
-  if (price < 0) {
+  if (price <= 0) {
     return next({
       status: 400,
       message: "Dish must have a price that is an integer greater than 0",
     });
   }
 
+  next();
+}
+
+function priceIsANumber(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (typeof(price) !== "number") {
+    return next({
+      status: 400,
+      message: "Dish price is not a number",
+    });
+  }
   next();
 }
 
@@ -71,8 +82,50 @@ function read(req, res, next) {
   res.status(200).json({ data: res.locals.foundDish });
 }
 
+function BodyIdRouteIdMatch(req, res, next) {
+  const { dishId } = req.params; // route id
+  const { data: { id } = {} } = req.body; // body id
+
+  if (dishId && id) {
+    if (dishId === id) {
+      return next();
+    }
+
+    next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+    });
+  }
+  next();
+}
+
+function update(req, res, next) {
+  const { data: { id, name, description, price, image_url } = {} } = req.body; // input
+  const data = req.body.data || {};
+
+  const dish = res.locals.foundDish; // original dish info
+
+  const requiredFields = ["name", "description", "price", "image_url"];
+
+  for (const field of requiredFields) {
+    if (data[field] !== dish[field]) {
+      dish[field] = data[field];
+    }
+  }
+
+  res.json({ data: dish });
+}
+
 module.exports = {
   list,
   create: [hasRequiredFields, priceGreaterThanZero, create],
   read: [dishIdExists, read],
+  update: [
+    dishIdExists,
+    hasRequiredFields,
+    priceIsANumber,
+    priceGreaterThanZero,
+    BodyIdRouteIdMatch,
+    update,
+  ],
 };
